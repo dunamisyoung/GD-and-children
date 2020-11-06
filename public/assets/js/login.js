337d74dbb563d9139525f3f28fc2d89d04c44288
dunamisyoung <pksd5693@gmail.com>
2020-11-06 10:36:56 +0900

    Feat: #1 - login 로그인기능구현
    1. 로그인폼 만들기
    2. 서버연동 구현
    3. 로그인 아이디/비밀번호 DB와 아이디/비밀번호 중복확인
    4. form태그 submit 기능구현 및 링크페이지이동

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,1 +1,55 @@
-// 로그인 페이지 js
+//Status
+const $loginForm = document.getElementById('login-form');
+const $loginBtn = document.querySelector('.login-btn');
+
+const $userId = document.getElementById('userId');
+const $userPw = document.getElementById('userPw');
+
+//DOMs
+
+// 로그인폼 서버와연결 기능구현
+const get = (url) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(JSON.parse(xhr.response));
+      } else {
+        reject(new Error(xhr.status));
+      }
+    };
+  });
+};
+
+// 로그인폼 필터링기능 구현
+$loginForm.onsubmit = (e) => {
+  e.preventDefault();
+
+  if (!$userId.value.trim()) {
+    alert('아이디를 입력해주세요');
+    $userId.focus();
+    return;
+  } else if (!$userPw.value.trim()) {
+    alert('비밀번호를 입력해주세요');
+    $userPw.focus();
+    return;
+  }
+
+  // 이벤트 발생시에 get함수 호출
+  get('/users')
+    .then((users) => {
+      // console.log(users);
+      const checkUser = users.find((user) => {
+        return user.id === $userId.value && user.pw === $userPw.value;
+      });
+      if (checkUser) {
+        location.assign('link.html');
+      } else {
+        alert('아이디와 비밀번호를 입력해주세요');
+      }
+    })
+    .catch();
+};
