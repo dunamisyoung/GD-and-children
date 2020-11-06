ed19c03f0c31838e33be40cfa382225c9c688b30
dongchan-K <ehdcks1028@gmail.com>
2020-11-06 15:47:05 +0900

    feat: #6 - 문제 만들기 버튼 및 참여코드 기능 구현

--- public/assets/js/link.js
+++ public/assets/js/link.js
@@ -1,1 +1,67 @@
 // 링크 입력 페이지 js
+
+// 페이지 로딩 시 , 로그인이 되어있지 않다면 login.html로 이동
+window.onload = e => {
+  if(!sessionStorage.getItem('login')){
+    alert('로그인이 필요합니다');
+    location.assign('/')
+  }
+}
+
+// Doms
+const $createQuestion = document.querySelector('.create-question');
+const $linkForm = document.getElementById('linkForm');
+const $linkInput = document.getElementById('linkInput');
+const $logOut = document.getElementById('logOut');
+
+// 세션 스토리지의 login을 키로 갖는 객체의 position 프로퍼티만 획득
+const {position} = JSON.parse(sessionStorage.getItem('login'));
+
+if(position === 'student') {
+  $createQuestion.style.display = 'none';
+}
+
+const get = url => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url);
+    xhr.send();
+
+    xhr.onload = () => {
+      if(xhr.status === 200) {
+        resolve(JSON.parse(xhr.response));
+      } else {
+        reject(new Error());
+      }
+    }
+  })
+}
+
+$linkForm.onsubmit = e => {
+  e.preventDefault();
+
+  get(`/question`)
+    .then((questions) => {
+      const linkValid = questions.find(question => $linkInput.value === question.id);
+      if(linkValid) {
+        sessionStorage.setItem('joinCode', `${$linkInput.value}`);
+        location.assign('/exam.html');
+      } else {
+        alert('올바른 참여 코드를 입력하세요');
+        $linkInput.focus();
+      }
+    })
+}
+
+$logOut.onclick = e => {
+  // log-out 시 세션 스토리지에서 
+  sessionStorage.removeItem('login');
+  location.assign('/')
+}
+
+
+
+
+
+
--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,1 +1,60 @@
 // 로그인 페이지 js
+//Status
+const $loginForm = document.getElementById('login-form');
+const $loginBtn = document.querySelector('.login-btn');
+
+const $userId = document.getElementById('userId');
+const $userPw = document.getElementById('userPw');
+
+//DOMs
+
+// 로그인폼 서버와연결 기능구현
+const get = (url) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(JSON.parse(xhr.response));
+      } else {
+        reject(new Error(xhr.status));
+      }
+    };
+  });
+};
+
+// 로그인폼 필터링기능 구현
+$loginForm.onsubmit = (e) => {
+  e.preventDefault();
+
+  if (!$userId.value.trim()) {
+    alert('아이디를 입력해주세요');
+    $userId.focus();
+    return;
+  } else if (!$userPw.value.trim()) {
+    alert('비밀번호를 입력해주세요');
+    $userPw.focus();
+    return;
+  }
+
+  // 이벤트 발생시에 get함수 호출
+  get('/users')
+    .then((users) => {
+
+      const checkUser = users.find((user) => {
+        return user.id === $userId.value && user.pw === $userPw.value;
+      });
+   
+      if (checkUser) {
+
+        // 세션 스토리지에 JSON 형식으로 login: {} 형태로 저장
+        sessionStorage.setItem('login', JSON.stringify({id: checkUser.id, position: checkUser.position, name: checkUser.name}));
+        location.assign('link.html');
+      } else {
+        alert('등록되지 않은 회원입니다');
+      }
+    })
+    .catch();
+};
