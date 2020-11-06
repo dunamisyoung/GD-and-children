a1023d918f2e277811a2bd552bfd436bae26449e
dunamisyoung <pksd5693@gmail.com>
2020-11-06 17:56:16 +0900

    Feat:#4 - exam
    1. 서버에 참여코드에 할당된문제받아로드/렌더링기능
    2. 총문제 갯수 확인 및 현재 푼 문제확인 기능
    3. 풀지않은 문제있으면 에러띄우는기능
    4. save 버튼누르면경고창을통해점수반환or 다시풀기기능

--- public/assets/js/exam.js
+++ public/assets/js/exam.js
@@ -1,1 +1,84 @@
-// 시험문제 풀기 페이지 js
+// State
+const $questionBox = document.querySelector('.question-box');
+const $title = document.querySelector('.title');
+const $saveBtn = document.querySelector('.save-btn');
+const $solvedQuestion = document.querySelector('.solved-question');
+const $questionForm = document.querySelector('.question-form');
+let $questionInputs = document.querySelectorAll('.question-input');
+
+const joinCode = 'QA332';
+
+const get = (url) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(JSON.parse(xhr.response));
+      } else {
+        reject(new Error(xhr.status));
+      }
+    };
+  });
+};
+
+get(`/question/${joinCode}`).then((question) => {
+  $title.textContent = question.title;
+
+  Object.keys(question).forEach((list, index) => {
+    if (index < 2) return;
+
+    $questionBox.innerHTML += `<div class="question-box">
+    <div class="question-content">
+      <div class="question-line">
+        <h2 class="question-title">
+          <span>Q-${`${question[list].id}`}</span>
+          ${`${question[list].question}`}
+        </h2>
+      </div>
+      <div class="question-line">
+        <p class="question-score">
+          <span>score</span>
+          ${`${question[list].score}`}
+        </p>
+      </div>
+      <div class="question-line">
+        <label for="questionAnswer-${`${question[list].id}`}" class="question-label"> answer </label>
+        <input type="text" id="questionAnswer-${`${question[list].id}`}" class="question-input" autocomplete="off" />
+      </div>
+    </div>
+  </div>`;
+  });
+  $questionInputs = document.querySelectorAll('.question-input');
+  $solvedQuestion.lastElementChild.textContent = $questionInputs.length;
+  $questionForm.addEventListener('focusout', (e) => {
+    let filledinputArr = [];
+    [...$questionInputs].forEach((questionInput) => {
+      if (questionInput.value.trim()) filledinputArr.push(questionInput);
+    });
+    $solvedQuestion.firstElementChild.textContent = filledinputArr.length;
+  });
+
+  $questionForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const warn = confirm('제출하시겠습니까?');
+    if (warn) {
+      let score = 0;
+
+      for (let i = 1; i <= $questionInputs.length; ++i) {
+        if (question[`Q${i}`]['answer'] === $questionInputs[i - 1].value) {
+          score += +question[`Q${i}`]['score'];
+        }
+      }
+      sessionStorage.setItem('joinCode', score);
+      alert('제출완료');
+      location.assign('result.html');
+    } else {
+      alert('다시제출해주세요');
+      return;
+    }
+  });
+});
